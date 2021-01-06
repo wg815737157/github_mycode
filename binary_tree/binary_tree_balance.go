1662065d009245fa63524a62115c17f75a99cb9f
wanggang <wanggang9@tal.com>
2021-01-06 18:32:55 +0800

    add

--- binary_tree/binary_tree_balance.go
+++ binary_tree/binary_tree_balance.go
@@ -17,3 +17,30 @@ func isBalanceTree(binaryTree *BinaryTree) bool {
 	}
 	return true
 }
+//从下到上是否是平衡树算法优化
+func isBalanceTree_2(binaryTree *BinaryTree) int {
+	if binaryTree == nil {
+		return 0
+	}
+	slNodeLen := isBalanceTree_2(binaryTree.slnode)
+	srNodeLen := isBalanceTree_2(binaryTree.srnode)
+	if slNodeLen < 0 {
+		fmt.Println("返回左侧", binaryTree.slnode.value)
+		return -1
+	}
+	if srNodeLen < 0 {
+		fmt.Println("返回右侧", binaryTree.srnode.value)
+		return -1
+	}
+	if slNodeLen-srNodeLen > 1 {
+		fmt.Println("不平衡节点是", binaryTree.value)
+		return -1
+	}
+	if srNodeLen-slNodeLen > 1 {
+		fmt.Println("不平衡节点是", binaryTree.value)
+	}
+	if slNodeLen > srNodeLen {
+		return slNodeLen + 1
+	}
+	return srNodeLen + 1
+}
